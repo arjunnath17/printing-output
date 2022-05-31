848163a735cd30219a65a4f8d8a64ccf49cf6d29
arjunnath17 <105217175+arjunnath17@users.noreply.github.com>
2022-05-31 16:27:16 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,41 @@
+let a=3;
+let b=5;
+let c;
+
+let result= 'let a=3;\n';
+  result+= 'let b=5;\n';
+  result+= 'let c;\n';
+  
+result+= '-------------------/n';
+result+= 'a+b='+(a+b)+'\n';
+result+= 'a-b='+(a-b)+'\n';
+result+= 'a*b='+(a*b)+'\n';
+result+= 'a/b='+(a/b)+'\n';
+result+= 'a%b='+(a%b)+'\n';
+result+= 'a+=b='+(a+=b)+'\n';
+result+= 'a-=b='+(a-=b)+'\n';
+result+= 'a*=b='+(a*=b)+'\n';
+result+= 'a/=b='+(a/=b)+'\n';
+result+= 'a%=b='+(a%=b)+'\n';
+result+= 'a==b='+(a==b)+'\n';
+result+= 'a!=b='+(a!=b)+'\n';
+result+= 'a>b='+(a>b)+'\n';
+result+= 'a<b='+(a<b)+'\n';
+result+= '!a&&!c='+(!a&&!c)+'\n';
+result+= '!a||!c='+(!a||!c)+'\n';
+
+
+
+alert (result);
+
+let first_name='Arjun';
+let last_name='Nath';
+let email='nath0022@algonquinlive.com';
+
+let output;
+output = 'My name is'+ first_name+' '+last_name+'.You can contact me at '+email+'.';
+alert(output);
+
+
+
+
